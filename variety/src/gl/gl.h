d2c21ef0881d60aba3e537eaa34004a6c52cc3dd
Ludvig Gunne Lindström <87268743+ludviggunne@users.noreply.github.com>
2022-02-27 21:00:42 +0100

    Comments for clarification concerning GLcall

--- variety/src/gl/gl.h
+++ variety/src/gl/gl.h
@@ -9,6 +9,8 @@ bool GLLogError(const char *file, int line, const char *func);
 
 
 #ifdef V_DEBUG
+	// Wrap OpenGL calls with this macro
+	// Declarations must be outside, since GLcall encloses expression in scope
 	#define GLcall(expr) { GLClearError(); expr; if(GLLogError(__FILE__, __LINE__, #expr)) { __debugbreak(); } }
 #else
 	#define GLcall(expr) expr
