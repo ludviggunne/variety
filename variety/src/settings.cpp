53ff925fd4cf952fdfdb586c4a13603e7e7757ab
Ludvig Gunne Lindström <87268743+ludviggunne@users.noreply.github.com>
2022-02-27 20:59:16 +0100

    Added settings

---
+++ variety/src/settings.cpp
@@ -0,0 +1,14 @@
+#include "settings.h"
+
+namespace Settings {
+
+// Initial parameters
+	// Camera
+	const float CameraInitXRotation = 0.0f;
+	const float CameraInitYRotation = 0.0f;
+	const float CameraInitDistance  = 10.0f;
+	const glm::vec3 CameraInitCenter = glm::vec3(0.0f, 0.0f, 0.0f);
+
+	const float CameraRotateSpeed = 0.01f;
+	const float CameraZoomSpeed = 1.0f;
+}
---
+++ variety/src/settings.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "glm/glm.hpp"
+
+namespace Settings {
+
+// Initial parameters
+	// Camera
+	extern const float CameraInitXRotation;
+	extern const float CameraInitYRotation;
+	extern const float CameraInitDistance;
+	extern const glm::vec3 CameraInitCenter;
+
+	extern const float CameraRotateSpeed;
+	extern const float CameraZoomSpeed;
+};
