7c1823209462d4f2f55440f7b699c2d9889815bc
Ludvig Gunne Lindström <87268743+ludviggunne@users.noreply.github.com>
2022-03-01 11:49:39 +0200

    Simple test program for shading / camera

--- variety/src/main.cpp
+++ variety/src/main.cpp
@@ -4,23 +4,82 @@
 
 #include "gl/gl.h"
 #include "GLFW/glfw3.h"
+#include "glm/ext.hpp"
 
 #include "gl/program.h"
 #include "gl/vertex_array.h"
+#include "gl/vertex.h"
+#include "camera.h"
+
+#define ASSERT(x) if(!(x)) __debugbreak();
+
+Camera camera;
+void scrollCallback(GLFWwindow *window, double xoffset, double yoffset) {
+	camera.OnScroll(yoffset);
+}
 
 int main() {
 
 	glfwInit();
+
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 	GLFWwindow *window = glfwCreateWindow(800, 800, "Test", NULL, NULL);
 
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
+	glfwSetScrollCallback(window, scrollCallback);
+
 	glfwSwapInterval(1);
 	glfwMakeContextCurrent(window);
 
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+	};
 
 	// GL objects scope
 	{
@@ -48,10 +107,60 @@ int main() {
 			__debugbreak();
 		}
 
+		ASSERT(program.SetUniform("uProj", glm::perspective(45.0f, 1.0f, .1f, 100.0f)));
+		//program.SetUniform("uProj", glm::mat4(1.0f));
+		
+		// Vertex array
+		VertexArray va;
+
+		const unsigned int nVert = sizeof(vertices) / sizeof(Vertex);
+		auto vertex_ptr = (Vertex *)vertices;
+		va.VertexData(vertex_ptr, nVert);
+
+		Index indices[nVert];
+		for (int i = 0; i < nVert; i++)
+			indices[i] = i;
+		va.IndexData(indices, nVert);
+
+
+		// Camera
+
+		GLcall(glEnable(GL_DEPTH_TEST));
+		GLcall(glViewport(0, 0, 800, 800));
+
+		bool mousePressed = false;
 		while (!glfwWindowShouldClose(window)) {
 
 			GLcall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
-			GLcall(glClear(GL_COLOR_BUFFER_BIT));
+			GLcall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+
+			program.Use();
+			ASSERT(program.SetUniform("uView", camera.GetProjection()));
+			ASSERT(program.SetUniform("uLightPos", camera.GetPosition()));
+
+			va.Bind();
+			va.Draw(GL_TRIANGLES);
+
+			// Handle input
+			if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+
+				double xpos, ypos;
+				glfwGetCursorPos(window, &xpos, &ypos);
+				glm::vec2 mousePos(static_cast<float>(xpos), static_cast<float>(ypos));
+
+
+				if (!mousePressed)
+				{
+					mousePressed = true;
+					camera.OnMousePress(mousePos, true, Camera::Action::Rotation);
+				}
+				else {
+					camera.OnMousePress(mousePos, false, Camera::Action::Rotation);
+				}
+			}
+			else {
+				mousePressed = false;
+			}
 
 			glfwSwapBuffers(window);
 			glfwPollEvents();
