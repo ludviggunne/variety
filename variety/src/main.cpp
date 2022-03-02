0b835e4b2e607f687e5269b1e6a37f26b060ae0c
Ludvig Gunne Lindström <87268743+ludviggunne@users.noreply.github.com>
2022-03-02 13:02:30 +0200

    Fun test with icosphere

--- variety/src/main.cpp
+++ variety/src/main.cpp
@@ -10,6 +10,9 @@
 #include "gl/vertex_array.h"
 #include "gl/vertex.h"
 #include "camera.h"
+#include "dual_contour.h"
+
+#include "gl/misc.h"
 
 #define ASSERT(x) if(!(x)) __debugbreak();
 
@@ -36,50 +39,6 @@ int main() {
 
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-
-	float vertices[] = {
-		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-
-		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-
-		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-
-		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-
-		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-
-		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-		 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
-	};
 
 	// GL objects scope
 	{
@@ -108,27 +67,84 @@ int main() {
 		}
 
 		ASSERT(program.SetUniform("uProj", glm::perspective(45.0f, 1.0f, .1f, 100.0f)));
-		//program.SetUniform("uProj", glm::mat4(1.0f));
 		
 		// Vertex array
 		VertexArray va;
 
-		const unsigned int nVert = sizeof(vertices) / sizeof(Vertex);
-		auto vertex_ptr = (Vertex *)vertices;
-		va.VertexData(vertex_ptr, nVert);
+		float t = (1.0f + sqrtf(5.0f)) / 2.0f;
 
-		Index indices[nVert];
-		for (int i = 0; i < nVert; i++)
-			indices[i] = i;
-		va.IndexData(indices, nVert);
+		glm::vec3 positions[] = {
+			glm::vec3{-1,  t,  0},
+			glm::vec3{ 1,  t,  0},
+			glm::vec3{-1, -t,  0},
+			glm::vec3{ 1, -t,  0},
+			glm::vec3{ 0, -1,  t},
+			glm::vec3{ 0,  1,  t},
+			glm::vec3{ 0, -1, -t},
+			glm::vec3{ 0,  1, -t},
+			glm::vec3{ t,  0, -1},
+			glm::vec3{ t,  0,  1},
+			glm::vec3{-t,  0, -1},
+			glm::vec3{-t,  0,  1}
+		};
 
+		std::vector<Vertex> verts;
+		auto addtri = [&verts, &positions](int i, int j, int k) {
+			PushTriangle(verts,
+				glm::normalize(positions[i]),
+				glm::normalize(positions[j]),
+				glm::normalize(positions[k]));
+		};
 
-		// Camera
+		addtri(0, 11, 5);
+		addtri(0, 5, 1);
+		addtri(0, 1, 7);
+		addtri(0, 7, 10);
+		addtri(0, 10, 11);
+		addtri(1, 5, 9);
+		addtri(5, 11, 4);
+		addtri(11, 10, 2);
+		addtri(10, 7, 6);
+		addtri(7, 1, 8);
+		addtri(3, 9, 4);
+		addtri(3, 4, 2);
+		addtri(3, 2, 6);
+		addtri(3, 6, 8);
+		addtri(3, 8, 9);
+		addtri(4, 9, 5);
+		addtri(2, 4, 11);
+		addtri(6, 2, 10);
+		addtri(8, 6, 7);
+		addtri(9, 8, 1);
+
+		va.VertexData(&verts[0], verts.size());
+
+		auto subdivide = [&verts, &va]() {
+			std::vector<Vertex> new_verts;
+			for (int i = 0; i < verts.size(); i += 3) {
+				auto p0 = glm::normalize(verts[i + 0].position);
+				auto p1 = glm::normalize(verts[i + 1].position);
+				auto p2 = glm::normalize(verts[i + 2].position);
+
+				auto m0 = glm::normalize(p0 + p1);
+				auto m1 = glm::normalize(p1 + p2);
+				auto m2 = glm::normalize(p2 + p0);
+
+				PushTriangle(new_verts, p0, m0, m2);
+				PushTriangle(new_verts, p1, m1, m0);
+				PushTriangle(new_verts, p2, m2, m1);
+
+				PushTriangle(new_verts, m0, m1, m2);
+			}
+			verts = new_verts;
+			va.VertexData(&verts[0], verts.size());
+		};
 
 		GLcall(glEnable(GL_DEPTH_TEST));
 		GLcall(glViewport(0, 0, 800, 800));
 
 		bool mousePressed = false;
+		bool spacePressed = false;
 		while (!glfwWindowShouldClose(window)) {
 
 			GLcall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
@@ -137,6 +153,7 @@ int main() {
 			program.Use();
 			ASSERT(program.SetUniform("uView", camera.GetProjection()));
 			ASSERT(program.SetUniform("uLightPos", camera.GetPosition()));
+			//ASSERT(program.SetUniform("uLightPos", glm::vec3(10.0f, 2.0f, 5.0f)));
 
 			va.Bind();
 			va.Draw(GL_TRIANGLES);
@@ -161,6 +178,14 @@ int main() {
 			else {
 				mousePressed = false;
 			}
+
+			if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+				if (!spacePressed) {
+					spacePressed = true;
+					subdivide();
+				}
+			}
+			else spacePressed = false;
 
 			glfwSwapBuffers(window);
 			glfwPollEvents();
