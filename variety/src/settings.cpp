b885c49be646caaf178babe3347e165bf7c00dcd
Ludvig Gunne Lindström <87268743+ludviggunne@users.noreply.github.com>
2022-03-02 21:48:19 +0200

    Update settings.cpp

--- variety/src/settings.cpp
+++ variety/src/settings.cpp
@@ -3,9 +3,9 @@
 namespace Settings {
 
 	// Camera
-	const float CameraInitXRotation  = 0.0f;
-	const float CameraInitYRotation  = 0.0f;
-	const float CameraInitDistance   = 10.0f;
+	const float CameraInitXRotation  = 15.0f;
+	const float CameraInitYRotation  = 15.0f;
+	const float CameraInitDistance   = 5.0f;
 	const glm::vec3 CameraInitCenter = glm::vec3(0.0f, 0.0f, 0.0f);
 
 	const float CameraRotateSpeed = 0.5f;
